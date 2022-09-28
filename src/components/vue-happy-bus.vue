f8b4f1863d48a5e58f6adfee633687c4d5455f76
kaka <Kaka.zhang@mspbots.ai>
2022-09-28 15:06:51 +0800

    vue happy bus

---
+++ src/components/vue-happy-bus.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    11122222
+    <el-button @click="click">emit</el-button>
+  </div>
+</template>
+ 
+ <script>
+import Vue from 'vue'
+import Bus from 'vue-happy-bus'
+Vue.prototype.$Bus = Bus
+export default {
+  data() {
+    return {
+      loading: true,
+      dis:'',
+      Bus: this.$Bus(this)
+    }
+  },
+  methods: {
+    click() {
+      this.Bus.$emit('bus','AAAAAAA')
+    }
+  },
+  created() {
+    this.Bus.$on('bus',(e)=>{
+      console.log(e)
+    })
+  }
+}
+
+</script>
+<style module>
+.red {
+  color: red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ export default new Router({
       component: () => import( /* webpackChunkName: 'Test' */ '@/components/test' )   //实现路由懒加载
     },
     {
+      path: '/test111',
+      name: 'Test111',
+      component: () => import( /* webpackChunkName: 'Test111' */ '@/components/vue-happy-bus' )   //实现路由懒加载
+    },
+    {
       path: '/gridlayout',
       name: 'Gridlayout',
       component: () => import( /* webpackChunkName: 'Gridlayout' */ '@/components/gridlayout' )  //实现路由懒加载
