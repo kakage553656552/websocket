189c1c7a7ea6eec5fd2f665b167ed203d48f71af
Kaka.zhang <Kaka.zhang@mspbots.ai>
2024-03-20 15:29:49 +0800

    配置本地开发代理

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,17 @@ module.exports = defineConfig({
         // 其他选项
       })
     ]
+  },
+  devServer: {
+    // 在这里添加 devServer 配置
+    port: 8080, // 指定开发服务器端口
+    host: 'localhost', // 指定开发服务器主机
+    proxy: {
+      // 配置代理
+      '/api': {
+        target: 'http://120.46.34.178',
+        changeOrigin: true
+      }
+    }
   }
 });
